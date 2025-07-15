47ce4c91566e8d322a934d705c2382e8c5251532
coderkeshav-yt <coderkeshavyt@gmail.com>
2025-07-15 12:37:24 +0530

    white_page

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/assets/' : '',
+  base: mode === 'production' ? '' : '',
   server: {
     host: "::",
     port: 8080,
@@ -34,9 +34,9 @@ export default defineConfig(({ mode }) => ({
         main: path.resolve(__dirname, 'index.html'),
       },
       output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
       }
     },
     minify: mode === 'production',
